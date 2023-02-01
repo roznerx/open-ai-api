d12c8b222b00453950c20e7c7298c47dca85a6c1
lautapercuspain <lautapercuspain@gmail.com>
2023-02-01 16:23:51 -0700

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,32 +3,22 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
-      <Link href="/" className="flex space-x-3">
-        <Image
-          alt="header text"
-          src="/writingIcon.png"
-          className="sm:w-12 sm:h-12 w-8 h-8"
-          width={32}
-          height={32}
-        />
-        <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
-          twitterBio.com
-        </h1>
-      </Link>
-      <a
-        href="https://vercel.com/templates/next.js/twitter-bio"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Image
-          alt="Vercel Icon"
-          src="/vercelLogo.png"
-          className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
-          width={32}
-          height={28}
-        />
-      </a>
-    </header>
+    <div className="bg-gradient-to-r from-indigo-500 text-center via-purple-500 to-pink-500 content-center flex w-full lg:w-full pb-7 px-2">
+      <header className="m-auto mt-5">
+        <Link href="/" className="flex space-x-3">
+          <Image
+            alt="Intelligent Code generator"
+            src="/gitIcon.png"
+            className="sm:w-12 sm:h-12 w-8 h-8"
+            width={32}
+            height={32}
+          />
+          <h1 className="sm:text-4xl text-2xl text-white ml-2 tracking-tight">
+          AIntelligentCode.com
+          </h1>
+        </Link>
+      </header>
+      
+      </div>
   );
 }
