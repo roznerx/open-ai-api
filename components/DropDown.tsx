7d6abcda8bedf2962d010a53ed8edda2769eafc4
lautapercuspain <lautapercuspain@gmail.com>
2023-02-02 21:34:45 -0700

    dropdown

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -15,22 +15,25 @@ export type ElementType = "Typescript" | "Javascript" | "React" | "Vue";
 interface DropDownProps {
   element: ElementType;
   setElement: (vibe: ElementType) => void;
-  elements: ElementType[]
+  elements: ElementType[];
 }
 
-
-export default function DropDown({elements, element, setElement }: DropDownProps) {
+export default function DropDown({
+  elements,
+  element,
+  setElement,
+}: DropDownProps) {
   return (
-    <Menu as="div" className="relative block text-left w-full">
+    <Menu as="div" className="relative mt-4 block w-full text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-between items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-black">
+        <Menu.Button className="inline-flex w-full items-center justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-black">
           {element}
           <ChevronUpIcon
             className="-mr-1 ml-2 h-5 w-5 ui-open:hidden"
             aria-hidden="true"
           />
           <ChevronDownIcon
-            className="-mr-1 ml-2 h-5 w-5 hidden ui-open:block"
+            className="-mr-1 ml-2 hidden h-5 w-5 ui-open:block"
             aria-hidden="true"
           />
         </Menu.Button>
@@ -58,12 +61,12 @@ export default function DropDown({elements, element, setElement }: DropDownProps
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                       element === item ? "bg-gray-200" : "",
-                      "px-4 py-2 text-sm w-full text-left flex items-center space-x-2 justify-between"
+                      "flex w-full items-center justify-between space-x-2 px-4 py-2 text-left text-sm",
                     )}
                   >
                     <span>{item}</span>
                     {element === item ? (
-                      <CheckIcon className="w-4 h-4 text-bold" />
+                      <CheckIcon className="text-bold h-4 w-4" />
                     ) : null}
                   </button>
                 )}
