707b3cd9fe0857bbf8ccb382f800fea118323089
lautapercuspain <lautapercuspain@gmail.com>
2023-02-08 11:57:08 -0600

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,52 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useSignInModal } from "./modals/SignInModal";
+import { AnimatePresence, motion } from "framer-motion";
+import { FADE_IN_ANIMATION_SETTINGS } from "@/lib/constants";
+import { useSession } from "next-auth/react";
+import useScroll from "hooks/use-scroll";
 
 export default function Header() {
+  const { SignInModal, setShowSignInModal } = useSignInModal();
+  const { data: session, status } = useSession();
+  const scrolled = useScroll(50);
   return (
     <>
       <Head>
         <title>A-Intelligent Code Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex w-full  content-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-2 pb-7 lg:w-full">
-        <header className="m-auto mt-5">
+      <SignInModal />
+      <div
+        className={`fixed top-0 w-full ${
+          scrolled
+            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+            : "bg-white/0"
+        } z-30 transition-all`}
+      >
+        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between xl:mx-auto">
           <Link href="/" className="flex space-x-3">
-            {/* <Image
-              alt="Intelligent Code generator"
-              src="/gitIcon.png"
-              className="h-8 w-8 sm:h-12 sm:w-12"
-              width={32}
-              height={32}
-            /> */}
-            <h1 className="ml-2 text-2xl tracking-tight text-white max-md:pt-4 max-sm:pt-0 sm:text-4xl">
+            <h1 className="ml-2 text-2xl tracking-tight text-black max-md:pt-4 max-sm:pt-0 sm:text-4xl">
               AIntelligent Code
             </h1>
           </Link>
-        </header>
+          <div>
+            <AnimatePresence>
+              {!session && status !== "loading" ? (
+                <motion.button
+                  className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+                  onClick={() => setShowSignInModal(true)}
+                  {...FADE_IN_ANIMATION_SETTINGS}
+                >
+                  Sign In
+                </motion.button>
+              ) : (
+                "User is logged in"
+              )}
+            </AnimatePresence>
+          </div>
+        </div>
       </div>
     </>
   );
