be718e1a1b20772bd5a1fa620ca70b1f64ead7eb
lautapercuspain <lautapercuspain@gmail.com>
2023-02-11 14:08:21 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
+};
+
+module.exports = nextConfig;
