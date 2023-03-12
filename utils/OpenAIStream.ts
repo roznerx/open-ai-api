fbf4ec5dcd4005046197c34298daa1aa4105ec74
lautapercuspain <lautapercuspain@gmail.com>
2023-03-11 22:04:59 -0300

    turbo API

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -4,6 +4,8 @@ import {
   ReconnectInterval,
 } from "eventsource-parser";
 
+type Messages = [any];
+
 export interface OpenAIStreamPayload {
   model: string;
   prompt: string;
@@ -15,6 +17,11 @@ export interface OpenAIStreamPayload {
   max_tokens: number;
   stream: boolean;
   n: number;
+}
+export interface OpenAITurboPayload {
+  model: string;
+  messages: any;
+  stream?: boolean;
 }
 
 export async function OpenAIStream(payload: OpenAIStreamPayload) {
@@ -74,4 +81,61 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
   });
 
   return stream;
+}
+export async function OpenAITurboStream(payload: OpenAITurboPayload) {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  let counter = 0;
+
+  const res: any = await fetch("https://api.openai.com/v1/chat/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      // callback
+      function onParse(event: ParsedEvent | ReconnectInterval) {
+        if (event.type === "event") {
+          const data = event.data;
+          // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
+          if (data === "[DONE]") {
+            controller.close();
+            return;
+          }
+          try {
+            const json = JSON.parse(data);
+            const text = json.choices[0].delta.content;
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+            counter++;
+          } catch (e) {
+            // maybe parse error
+            controller.error(e);
+          }
+        }
+      }
+
+      // stream response (SSE) from OpenAI may be fragmented into multiple chunks
+      // this ensures we properly read chunks and invoke an event for each SSE event stream
+      const parser = createParser(onParse);
+      // https://web.dev/streams/#asynchronous-iteration
+      for await (const chunk of res.body as any) {
+        if (chunk == "```") {
+          console.log("🚀 - chunk:", chunk);
+          console.log("WAEEEEE");
+        }
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+
+  return stream;
+
+  // Log the generated text from the GPT-3 and GPT-3.5-Turbo models to the console
 }
