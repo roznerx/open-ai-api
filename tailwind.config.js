65ad0858626d4244b731d2ef6a6fcd94d07509ce
lautapercuspain <lautapercuspain@gmail.com>
2023-03-11 22:02:13 -0300

    theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   future: {
     hoverOnlyWhenSupported: true,
   },
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -10,17 +11,30 @@ module.exports = {
   ],
   theme: {
     screens: {
-      sm: '640px',
-  
-      md: '768px',
-  
-      lg: '1024px',
-  
-      xl: '1280px',
-  
-      '2xl': '1536px',
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: "#E9E9EB",
+          200: "#D4D3D8",
+          300: "#A9A8B0",
+          400: "#93929C",
+          500: "#525162",
+          600: "#2F2E49",
+          700: "#272639",
+        },
+        mint: "#A1FFE0",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
 };
