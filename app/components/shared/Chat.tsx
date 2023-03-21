90e8efa517bc6bfc140baf9300b8b324e4196e15
lautapercuspain <lautapercuspain@gmail.com>
2023-03-21 00:28:08 -0300

    Awesome Chat!

--- app/components/shared/Chat.tsx
+++ app/components/shared/Chat.tsx
@@ -1,3 +1,4 @@
+import "./chat.css"
 import {
   ChatContainer,
   MainContainer,
@@ -6,6 +7,7 @@ import {
 } from "@chatscope/chat-ui-kit-react"
 import { Send } from "lucide-react"
 import tailwindConfig from "tailwind.config.js"
+import { parseText } from "utils/parseText"
 import GenerateCode from "../GenerateCode"
 
 export default function Chat({
@@ -18,59 +20,49 @@ export default function Chat({
 }) {
   const colors: any = tailwindConfig.theme?.extend?.colors
 
-  return (
-    <MainContainer className="mb-14 flex min-h-screen min-w-[100%] flex-col justify-end overflow-scroll rounded-md">
-      {generatedResponse.length > 0 && (
-        <ChatContainer
-          className="min-w-[100%];
-        "
-        >
-          <MessageList className="w-[80%] min-w-[100%]">
-            {generatedResponse.map((generatedMessage) => {
-              let finalCode
-              // console.log("generatedMessage:", generatedMessage)
-              let matchData = [...generatedMessage.matchAll(/```/g)]
+  const CodeMessages = () => {
+    return generatedResponse.map((generatedMessage) => {
+      const result = parseText(generatedMessage)
+      console.log("result:", result)
 
-              const codeString = Array.from(matchData, (m) => m[1]).join("```")
-              console.log("code string Data: ", codeString)
-              console.log("matchData: ", matchData)
-              const hash = generatedMessage.indexOf("```")
-              if (hash !== -1) {
-                const customCode = generatedMessage.substring(hash)
-                const secondIndex = customCode.substring(3).indexOf("```")
-                finalCode = generatedMessage.substring(
-                  hash,
-                  matchData[1]?.["index"],
-                )
-                console.log("finalCode: ", finalCode.substring(3))
-              }
-
-              return finalCode ? (
-                <GenerateCode
-                  align="start"
-                  generatedCode={finalCode.replace("tsx", "").substring(3)}
-                />
-              ) : (
+      return result.length
+        ? result.map((item: any) => {
+            if (item.hasOwnProperty("text")) {
+              return (
                 <Message
+                  className="my-2"
                   model={{
-                    message: generatedMessage,
+                    message: item.text,
                     direction: "incoming",
                     position: "normal",
                   }}
                 />
               )
-            })}
+            } else {
+              return <GenerateCode align="start" generatedCode={item.code} />
+            }
+          })
+        : null
+    })
+  }
+
+  return (
+    <MainContainer className="flex max-h-[550px] min-w-[100%] flex-col overflow-scroll rounded-md">
+      {generatedResponse.length > 0 && (
+        <ChatContainer className="min-w-[100%]">
+          <MessageList className="w-[80%] min-w-[100%] bg-gray-300 dark:bg-gray-900">
+            <CodeMessages />
           </MessageList>
         </ChatContainer>
       )}
-      <div className="fixed bottom-4 left-0 right-0 m-auto w-[76%]">
+      <div className="fixed bottom-4 left-0 right-0 m-auto w-[82%]">
         <textarea
+          className="w-[100%] resize-none rounded-md p-1 pl-1 font-mono placeholder:pl-2 placeholder:pt-1 placeholder:text-sm placeholder:text-slate-200 focus:border-transparent focus:ring-black/30 dark:bg-slate-700 dark:text-slate-200"
           value={codeSentence}
           onChange={(e) => setCodeSentence(e.target.value)}
           onKeyDown={(e) => onCodeGeneration(e)}
-          rows={2}
-          className="h-8 w-[100%] resize-none rounded-md p-0 placeholder:pl-2 placeholder:pt-1 placeholder:text-sm placeholder:text-slate-200 focus:border-transparent focus:ring-black/30 dark:bg-slate-700 dark:text-slate-200"
-          placeholder={"type message here"}
+          rows={1}
+          placeholder={"type a code idea here 🤓"}
         ></textarea>
         {useArrow && (
           <button className="absolute right-1 mt-1 rounded-md p-1 text-gray-500 hover:bg-gray-100 disabled:hover:bg-transparent dark:hover:bg-gray-900 dark:hover:text-gray-400 dark:disabled:hover:bg-transparent md:bottom-2.5 md:right-2">
