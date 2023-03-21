1d70b99e7cd0003e95ddf633286a05a0b47faa66
lautapercuspain <lautapercuspain@gmail.com>
2023-03-21 00:27:51 -0300

    Code Blocks

--- app/components/GenerateCode.tsx
+++ app/components/GenerateCode.tsx
@@ -14,26 +14,21 @@ export default function GenerateCode({
   return (
     <>
       <div
-        className={`w-4/4 flex flex-col items-${align} md:items-${align} lg:items-${align}`}
+        className={`my-2 flex flex-col items-${align} md:items-${align} lg:items-${align}`}
       >
         {generatedCode
           .substring(generatedCode.indexOf("**") + 0)
           .split("**::")
           .map((generated) => {
             return (
-              <div
-                className="flex flex-wrap rounded-xl border bg-white  text-left font-mono shadow-md transition hover:bg-gray-100"
-                key={generated}
-              >
-                <div className="overflow-x-auto sm:text-left lg:max-w-2xl">
-                  <CopyBlock
-                    showLineNumbers
-                    text={generated}
-                    language={langElement === "Typescript" ? "tsx" : "jsx"}
-                    codeBlock
-                    theme={dracula}
-                  />
-                </div>
+              <div className="w-full text-left">
+                <CopyBlock
+                  showLineNumbers
+                  text={generated}
+                  language={langElement === "Typescript" ? "tsx" : "jsx"}
+                  codeBlock
+                  theme={dracula}
+                />
               </div>
             )
           })}
