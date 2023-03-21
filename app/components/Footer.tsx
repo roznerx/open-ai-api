a3ffbbd2816104b4bc4b5cec887c4901d061989b
lautapercuspain <lautapercuspain@gmail.com>
2023-03-21 19:27:26 -0300

    bg color

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   const pathName = usePathname()
   const showFooter = pathName !== "/code-chat"
   return showFooter ? (
-    <footer className="pt-4sm:mb-0 mt-5 mb-3 flex h-16 w-full flex-col items-center space-y-3 px-3 sm:h-20 sm:flex-row sm:pt-2">
+    <footer className="flex h-16 w-full flex-col items-center  space-y-3 bg-gradient-to-b from-purple-800 via-purple-700 to-purple-600 px-3 text-white">
       <div className="m-auto content-center">
         <p>AI - Intelligent Code ®</p>
       </div>
