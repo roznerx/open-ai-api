1064c9b308fc0dbe6406d0edb011946bbd04a752
lautapercuspain <lautapercuspain@gmail.com>
2023-03-22 19:54:41 -0300

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   const pathName = usePathname()
   const showFooter = pathName !== "/code-chat"
   return showFooter ? (
-    <footer className="flex h-16 w-full flex-col items-center  space-y-3 bg-gradient-to-b from-purple-800 via-purple-700 to-purple-600 px-3 text-white">
+    <footer className="flex h-16 w-full flex-col items-center  space-y-3  px-3 text-white">
       <div className="m-auto content-center">
         <p>AI - Intelligent Code ®</p>
       </div>
