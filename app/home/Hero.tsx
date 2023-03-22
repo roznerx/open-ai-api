2bfd64690a8c079f9baa0883f03f27433f96b79c
lautapercuspain <lautapercuspain@gmail.com>
2023-03-22 19:55:28 -0300

    styles

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -7,15 +7,15 @@ export default function Feature() {
   return (
     <>
       <section>
-        <h1 className="block text-center text-5xl font-bold text-gray-700 dark:text-white">
-          Create <p className="inline-block text-mint">Genius Code</p>
+        <h1 className="p-3 text-center text-5xl font-bold text-white dark:text-white">
+          Create <p className="inline-block  text-mint">Genius Code</p>
         </h1>
         <p className="mt-2 px-3 text-center text-white">
           Create Better, Faster and Easier Code with your AI Genius.
         </p>
       </section>
       <section className="mt-20">
-        <h1 className="mx-auto block max-w-md text-center text-5xl font-bold text-gray-700 dark:text-white">
+        <h1 className="mx-auto block max-w-md text-center text-5xl font-bold text-white dark:text-white">
           Use it with your favorite libraries
         </h1>
         <p className="mx-auto mt-3 max-w-lg px-3 text-center text-white">
