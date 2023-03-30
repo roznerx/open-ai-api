5700ce4246052069e42ff1269b29fd386e278657
lautapercuspain <lautapercuspain@gmail.com>
2023-03-29 23:25:59 -0300

    clean up

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link"
 import { useSignInModal } from "./modals/SignInModal"
 import { LSConfig } from "@/lib/constants"
-import useScroll from "hooks/use-scroll"
 import UserDropdown from "app/components/auth/UserDropdown"
 import { useEffect } from "react"
 import useLocalStorage from "hooks/use-localstorage"
@@ -16,8 +15,8 @@ const rubik = Rubik({
 })
 
 export default function Header({ session }) {
+  // const pathname = usePathname()
   const { SignInModal, setShowSignInModal } = useSignInModal()
-  const scrolled = useScroll(50)
   const [userId, setUserId] = useLocalStorage(LSConfig.user.userId, "")
   useEffect(() => {
     setUserId(session?.user?.id)
@@ -31,15 +30,6 @@ export default function Header({ session }) {
         <div className="flex max-w-screen-xl items-center justify-between xl:mx-auto">
           <div className="ml-7">
             <Link href="/" className="mx-auto mt-3 ml-7 flex sm:mx-0">
-              {window.location.pathname === "/" && (
-                <Image
-                  alt="Code Genius"
-                  src={"/logo.svg"}
-                  width={24}
-                  height={24}
-                  className="mb-8"
-                />
-              )}
               <h1
                 className={`${rubik.variable} text-lg sm:text-xl ml-2 font-rubik text-2xl font-bold leading-6 tracking-tight text-white dark:text-white max-md:pt-4 max-sm:pt-0`}
               >
