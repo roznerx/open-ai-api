2f6e2cf54947148652b95c0de0b419bbf8e6775f
unknown <ignaciorosner@gmail.com>
2023-04-05 00:04:52 -0300

    Finished mobile design for modal

--- app/components/modals/SignInModal.tsx
+++ app/components/modals/SignInModal.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { signIn } from "next-auth/react"
-import { useState, Dispatch, SetStateAction, useCallback, useMemo } from "react"
-import Image from "next/image"
-import BaseModal from "app/components/modals/BaseModal"
-import { Poppins } from "next/font/google"
+import { signIn } from "next-auth/react";
+import { useState, Dispatch, SetStateAction, useCallback, useMemo } from "react";
+import Image from "next/image";
+import BaseModal from "app/components/modals/BaseModal";
+import { Poppins } from "next/font/google";
 import GithubLogo from 'public/icons/github.svg';
 import GmailLogo from 'public/icons/gmail.svg';
+import useWindowSize from "hooks/use-window-size";
 
 const popins = Poppins({
   variable: "--font-popins",
@@ -25,76 +26,79 @@ const SignInModal = ({
   const [signInClickedGitHub, setSignInClickedGitHub] = useState<boolean>(false);
   const [signInClickedGoogle, setSignInClickedGoogle] = useState<boolean>(false);
 
+  const { windowSize, isMobile, isDesktop } = useWindowSize();
+
   return (
     <BaseModal showModal={/*showSignInModal*/true} setShowModal={setShowSignInModal}>
       {/* MAIN DIV - BACKGROUND*/}
-      <div className="rounded-2xl h-[487px] w-[504.01px] bg-purple-700 p-4">
+      <div className="rounded-2xl sm:h-[487px] sm:w-[504.01px] bg-purple-700 p-4">
         {/* INNER DIV - FLEX CONTAINER */}
         { /* TITLE + SUBTITLE */ }
         <div className="flex flex-col content-center justify-start justify-items-start gap-4 sm:p-12">
-          <div className="flex flex-col content-center justify-start justify-items-start gap-4 mb-8">
-            <h1 className={`${popins.variable} text-center text-[28px] font-[700] leading-6 text-white sm:text-left`}>
+          <div className="flex flex-col content-center justify-start justify-items-start gap-4 mb-4 sm:mb-8 mt-8 sm:mt-0">
+            <h1 className={`${popins.variable} text-center text-[28px] sm:text-[30px] font-[700] leading-6 text-white sm:text-left`}>
               Create your Account
             </h1>
-            <h6 className="text-[20px] font-[400] font-mono leading-10 text-gray-200">
+            <h6 className="text-[16px] sm:text-[20px] font-[400] font-mono leading-10 text-gray-200 text-center sm:text-left">
               Start coding with Code Genius
             </h6>
           </div>
           {/* DIVIDER */}
-          <hr className="border-1 border-purple-500 w-[384.01px]" />
-          <div className="flex flex-col content-center justify-start justify-items-start gap-4 mb-8">
+          <hr className="border-1 border-purple-500 sm:w-[384.01px]" />
+          {/* BUTTONS */}
+          <div className={`flex ${isMobile ? 'flex-row' : 'flex-col'} content-center justify-start justify-items-start gap-4 mb-8`}>
             {/* GITHUB BUTTON */}
-          <button
-            disabled={signInClickedGitHub}
-            className={"h-[80px] w-[380px] bg-black rounded-lg"}
-            onClick={() => {
-              setSignInClickedGitHub(true)
-              signIn("github", {
-                callbackUrl: process.env.NEXTAUTH_URL,
-              })
-            }}
-          >
-            {
-              signInClickedGitHub ? 
-              <p className="text-white text-[28px] font-[700]">Loading...</p> : 
-              <div className="flex inline-flex content-center justify-center justify-items-center gap-2">
-                <Image
-                  width={32}
-                  height={32}
-                  alt="Github Logo"
-                  src={GithubLogo.src}
-                  className="h-[25.51px] w-[26.15px] flex self-center"
-                />
-                <p className="text-white text-[28px] font-[700]">GitHub</p>
-              </div>
-            }
-          </button>
-          {/* GOOGLE BUTTON */}
-          <button
-            disabled={signInClickedGoogle}
-            className={"h-[80px] w-[380px] bg-white rounded-lg"}
-            onClick={() => {
-              setSignInClickedGoogle(true)
-              signIn("google", {
-                callbackUrl: process.env.NEXTAUTH_URL,
-              })
-            }}
-          >
-            {
-              signInClickedGoogle ? 
-              <p className="text-gray-800 text-[28px] font-[700]">Loading...</p> : 
-              <div className="flex inline-flex content-center justify-center justify-items-center gap-2">
-                <Image
-                  width={32}
-                  height={32}
-                  alt="Gmail Logo"
-                  src={GmailLogo.src}
-                  className="h-[38px] w-[38x] flex self-center"
-                />
-                <p className="text-gray-800 text-[28px] font-[700]">Gmail</p>
-              </div>
-            }
-          </button>
+            <button
+              disabled={signInClickedGitHub}
+              className={"h-[40px] w-full sm:h-[80px] sm:w-[380px] bg-black rounded-lg"}
+              onClick={() => {
+                setSignInClickedGitHub(true)
+                signIn("github", {
+                  callbackUrl: process.env.NEXTAUTH_URL,
+                })
+              }}
+            >
+              {
+                signInClickedGitHub ? 
+                <p className="text-white text-[28px] font-[700]">Loading...</p> : 
+                <div className="flex inline-flex content-center justify-center justify-items-center gap-2">
+                  <Image
+                    width={32}
+                    height={32}
+                    alt="Github Logo"
+                    src={GithubLogo.src}
+                    className="h-[25.51px] w-[26.15px] flex self-center"
+                  />
+                  <p className="text-white text-[28px] font-[700]">GitHub</p>
+                </div>
+              }
+            </button>
+            {/* GOOGLE BUTTON */}
+            <button
+              disabled={signInClickedGoogle}
+              className={"h-[40px] w-full sm:h-[80px] sm:w-[380px] bg-white rounded-lg"}
+              onClick={() => {
+                setSignInClickedGoogle(true)
+                signIn("google", {
+                  callbackUrl: process.env.NEXTAUTH_URL,
+                })
+              }}
+            >
+              {
+                signInClickedGoogle ? 
+                <p className="text-gray-800 text-[28px] font-[700]">Loading...</p> : 
+                <div className="flex inline-flex content-center justify-center justify-items-center gap-2">
+                  <Image
+                    width={32}
+                    height={32}
+                    alt="Gmail Logo"
+                    src={GmailLogo.src}
+                    className="h-[38px] w-[38x] flex self-center"
+                  />
+                  <p className="text-gray-500 text-[28px] font-[700]">Gmail</p>
+                </div>
+              }
+            </button>
           </div>
         </div>
       </div>
