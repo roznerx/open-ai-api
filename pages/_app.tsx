582cd58bbdd3508e0ca990d6d9042c3f079885f5
lautapercuspain <lautapercuspain@gmail.com>
2023-04-18 10:27:55 -0700

    remove analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
-import { Analytics } from "@vercel/analytics/react";
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import type { AppProps } from "next/app"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
