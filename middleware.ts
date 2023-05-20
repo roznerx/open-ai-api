0f44aa5502840b86ac89cee6da7d03e88e803b99
lautapercuspain <lautapercuspain@gmail.com>
2023-05-20 10:29:37 -0700

    middelware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,15 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export async function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+
   let ip = request.ip ?? request.headers.get("x-real-ip")
+  // console.log("Headers:", request.headers)
+  // console.log("ip:", ip)
   const forwardedFor = request.headers.get("x-forwarded-for")
   if (!ip && forwardedFor) {
     ip = forwardedFor.split(",").at(0) ?? "Unknown"
   }
-
-  const response = NextResponse.next()
 
   if (ip) {
     response.cookies.set("user-ip", ip, {
