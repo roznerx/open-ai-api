7db6fa6c6121634493db9e04f8c1d90cfea4f735
lautapercuspain <lautapercuspain@gmail.com>
2023-05-21 14:11:05 -0700

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,12 @@
 export default async function sitemap() {
-  const routes = ["", "/code-idea", "/code-chat", "/pricing"].map((route) => ({
+  const routes = [
+    "",
+    "/code-idea",
+    "/code-chat",
+    "/pricing",
+    "/privacy",
+    "/terms-and-conditions",
+  ].map((route) => ({
     url: `https://www.code-genius.dev${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }))
