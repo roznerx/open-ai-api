ed97cb72d383e5dad2ee3d2f9b69860d16c96c87
lautapercuspain <lautapercuspain@gmail.com>
2023-06-09 11:45:09 -0700

    clean

--- app/code-chat/client.tsx
+++ app/code-chat/client.tsx
@@ -85,11 +85,7 @@ export default function Client({ session }) {
   return (
     <>
       <Header session={session} setShowSignInModal={setShowSignInModal} />
-      <div className="absolute top-16 z-30 w-full bg-transparent sm:top-12">
-        <h2 className="mx-auto flex w-full items-start justify-center text-3xl text-gray-200 sm:text-5xl">
-          Chat with a coding expert
-        </h2>
-      </div>
+
       {/* Chat Container */}
       <Chat
         generatedResponse={generatedMessages}
