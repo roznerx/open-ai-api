fe6834b8f2626eb07052919f6ded181143dde9ea
lautapercuspain <lautapercuspain@gmail.com>
2023-06-09 11:44:49 -0700

    tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
         {
           lineHeight: "34px",
           fontWeight: "400",
-          letterSpacing: "0.5px",
+          letterSpacing: "-0.02em",
         },
       ],
       "3xl": [
@@ -39,7 +39,7 @@ module.exports = {
         },
       ],
       "4xl": [
-        "48px",
+        "40px",
         {
           lineHeight: "58.09px",
           fontWeight: "600",
@@ -47,7 +47,7 @@ module.exports = {
         },
       ],
       "5xl": [
-        "44px",
+        "48px",
         {
           lineHeight: "45.3px",
           letterSpacing: "-0.02em",
