678f666aa50082348dda77459b70458990225077
lautapercuspain <lautapercuspain@gmail.com>
2023-06-16 13:30:40 -0700

    test open ai functions

---
+++ app/movies/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+export default function Page() {
+  return (
+    <>
+      {/* <main className="flex min-h-screen w-screen px-4 text-center">
+        <Chat
+          loading={loading}
+          
+          codeSentence={codeSentence}
+          generatedResponse={generatedMessages}
+          setCodeSentence={setCodeSentence}
+        />
+        <InputChat
+          inputRef={inputRef}
+          codeSentence={codeSentence}
+          setCodeSentence={setCodeSentence}
+          onCodeGeneration={onCodeGeneration}
+          onArrowPress={onArrowPress}
+        />
+      </main> */}
+    </>
+  )
+}
