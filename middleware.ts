724d74c85e8834b6887352eb7bd48a18ece20289
aintelligentcode <lautapercuspain@gmail.com>
2023-06-25 11:51:39 -0700

    middelware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server"
+import { getLocale } from "utils/getLocale"
+
+let locales = ["en-US", "es", "pt"]
 
 export async function middleware(request: NextRequest) {
+  const acceptLangHeader = request.headers.get("accept-language")
+
   const response = NextResponse.next()
 
   let ip = request.ip ?? request.headers.get("x-real-ip")
-  // console.log("Headers:", request.headers)
-  // console.log("ip:", ip)
   const forwardedFor = request.headers.get("x-forwarded-for")
   if (!ip && forwardedFor) {
     ip = forwardedFor.split(",").at(0) ?? "Unknown"
@@ -16,5 +19,22 @@ export async function middleware(request: NextRequest) {
       httpOnly: false,
     })
   }
+
+  // Check if there is any supported locale in the pathname
+  const pathname = request.nextUrl.pathname
+  const pathnameIsMissingLocale = locales.every(
+    (locale) =>
+      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+  )
+
+  // Redirect if there is no locale
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(acceptLangHeader as string, locales)
+
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url))
+  }
+
   return response
 }
