be608d2b78e91cbfba0849099d4cee8a9bfc4fbd
aintelligentcode <lautapercuspain@gmail.com>
2023-07-07 19:42:21 -0700

    faqs

--- app/pricing/faqs.tsx
+++ app/pricing/faqs.tsx
@@ -54,6 +54,25 @@ export default function Faqs({ translations: faqs }) {
             <>
               <Disclosure.Button className="text-sm group flex w-full justify-between rounded-lg border border-purple-900 bg-purple-800 px-4 py-2 text-left font-medium text-purple-900 hover:bg-purple-900 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                 <span className="text-gray-300 group-hover:text-white">
+                  {faqs["2.a"].title}
+                </span>
+                <ChevronUp
+                  className={`${
+                    open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-white`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="text-sm px-4 pt-4 pb-2 text-center text-gray-200 sm:text-left ">
+                {faqs["2.a"].desc}
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="text-sm group flex w-full justify-between rounded-lg border border-purple-900 bg-purple-800 px-4 py-2 text-left font-medium text-purple-900 hover:bg-purple-900 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span className="text-gray-300 group-hover:text-white">
                   {faqs["2.1"].title}
                 </span>
                 <ChevronUp
