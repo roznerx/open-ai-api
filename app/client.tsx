6bb4e8e7b34bfedb534e255dff5378ed73f8ec50
lautapercuspain <lautapercuspain@gmail.com>
2023-07-31 12:58:26 -0700

    client

--- app/client.tsx
+++ app/client.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useSignInModal } from "./components/modals/SignInModal"
+
 import SuperHero from "./home/SuperHero"
 
 import Hero from "./home/Hero"
@@ -8,12 +10,28 @@ import ShowCaseCard from "./home/ShowCaseCard"
 import Feature from "./home/Feature"
 import HomeChat from "./home/HomeChat"
 
-export default function Client({ translations, session }) {
+export default function Client({
+  translations,
+  session,
+  ip,
+  apiCalls,
+  loggedUserData,
+}) {
+  const { setShowSignInModal } = useSignInModal({})
+
   return (
     <>
       <div className="absolute inset-0 animate-pulseCustom before:absolute before:inset-0 before:block before:h-full before:w-full before:rounded-full before:bg-gradient-to-br before:from-mint/30 before:to-blue/20 before:blur-[120px] before:content-[''] md:mx-auto md:h-[750px] md:w-[1250px] lg:flex"></div>
       <SuperHero translations={translations?.home?.superHero} />
-      <HomeChat translations={translations?.home?.chat} session={session} />
+      <HomeChat
+        setShowSignInModal={setShowSignInModal}
+        translations={translations?.home?.chat}
+        creditsModalTranslations={translations?.modals?.moreCredits}
+        ip={ip}
+        apiCalls={apiCalls}
+        session={session}
+        loggedUserData={loggedUserData}
+      />
       <Hero />
       <ShowCaseCard
         videoSrc="static/side-by-side-epic.mp4"
