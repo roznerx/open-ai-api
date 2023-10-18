cd077ae6ec70042bed96aa00ea46aa76df7c7340
lautapercuspain <lautapercuspain@gmail.com>
2023-10-18 11:13:39 -0700

    latest

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -67,7 +67,7 @@ export default async function Dashboard({
             </p>
           </div>
           <Client
-            subscriptionId={stripeSession.subscription}
+            subscriptionId={stripeSession?.subscription}
             isPremium={session?.user?.isPremium}
             session={session}
             translations={dictionary}
