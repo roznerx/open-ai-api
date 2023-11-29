2d56aaeb8495a8bbeed9a5ddc25bf4d54d5d5f62
Lautaro Gruss <lautapercuspain@gmail.com>
2023-11-29 13:55:57 -0700

    Collapsable Sidebar (#30)
    * latest

--- app/components/auth/UserDropdown.tsx
+++ app/components/auth/UserDropdown.tsx
@@ -1,18 +1,43 @@
 "use client"
 
+import Feedback from "../Feedback"
 import UserMenu from "./UserMenu"
 
-export default function UserDropdown({ session, translations }) {
+export default function UserDropdown({
+  session,
+  translations,
+  setShowWidget,
+  showWidget,
+}) {
   const { email, image } = session?.user || {}
 
   if (!email) return null
 
   return (
-    <UserMenu
-      translations={translations}
-      session={session}
-      email={email}
-      image={image}
-    />
+    <>
+      {session && (
+        <div className="mr-14 mt-1 hidden flex-col items-end transition-all sm:flex ">
+          <button
+            onClick={() => setShowWidget((prev) => !prev)}
+            className="mr-3 mt-1 flex h-4 w-28 items-center justify-center rounded-lg border
+                   border-gray-300 bg-purple-900 p-4 text-gray-200 hover:cursor-pointer hover:text-gray-50"
+          >
+            <span>{translations?.feedback?.title}</span>
+          </button>
+          <Feedback
+            translations={translations?.feedback}
+            session={session}
+            setShowWidget={setShowWidget}
+            showWidget={showWidget}
+          />
+        </div>
+      )}
+      <UserMenu
+        translations={translations}
+        session={session}
+        email={email}
+        image={image}
+      />
+    </>
   )
 }
