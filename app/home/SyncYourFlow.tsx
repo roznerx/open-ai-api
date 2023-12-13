2702a6a4ecbd56f349f27def3ca639fffc15bec1
lautapercuspain <lautapercuspain@gmail.com>
2023-12-12 20:49:54 -0700

    yo

--- app/home/SyncYourFlow.tsx
+++ app/home/SyncYourFlow.tsx
@@ -1,10 +1,10 @@
 export default function SyncYourFlow() {
   return (
-    <div className="mx-auto my-10  w-screen ">
+    <div className="mx-auto my-10 w-screen">
       <p className="mt-5 flex justify-center text-center text-2xl uppercase text-[#B095FF]">
         BONUS GITHUB APP TO SYNC WITH YOUR WORKFLOW
       </p>
-      <h5 className="my-4 flex justify-center text-4xl text-white ">
+      <h5 className="my-4 flex justify-center text-4xl text-white">
         Sync with your workflow
       </h5>
       {/* <p className="mt-5 flex justify-center text-center text-2xl text-white">
@@ -13,7 +13,7 @@ export default function SyncYourFlow() {
       <div
         className="mx-auto grid h-auto w-[90%] grid-cols-1
       rounded-xl bg-gradient-to-b from-[#B095FF] via-[#8ABFE5] to-[#B1EAF1] p-4 sm:h-[451px]
-      sm:w-[75%] sm:grid-cols-2 sm:p-0"
+      sm:w-[72%] sm:grid-cols-2 sm:p-0"
       ></div>
     </div>
   )
