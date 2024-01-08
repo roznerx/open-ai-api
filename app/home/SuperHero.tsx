707ae03224bac3898ac3da19b8e69f566e499bb0
lautapercuspain <lautapercuspain@gmail.com>
2024-01-08 08:41:08 -0700

    clean up

--- app/home/SuperHero.tsx
+++ app/home/SuperHero.tsx
@@ -12,19 +12,6 @@ export default function SuperHero({ translations }) {
          text-white sm:text-3xl"
         >
           {translations.subtitle}
-          <span className="relative">
-            <span className="absolute left-2 top-1 ml-2 inline-flex sm:top-0">
-              <span className="absolute top-0 sm:relative sm:top-0">
-                <span className="absolute -top-2 right-[2px] text-[10px] text-mint">
-                  ✦
-                </span>
-                <span className="text-[12px] text-mint">✦</span>
-                <span className="absolute bottom-2 left-2 text-[16px] text-mint">
-                  ✦
-                </span>
-              </span>
-            </span>
-          </span>
         </p>
       </section>
     </>
