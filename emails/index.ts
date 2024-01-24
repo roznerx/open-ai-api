1ffa237ef0626aadb2f75a846b6e4910b8a5c541
lautapercuspain <lautapercuspain@gmail.com>
2024-01-23 20:15:50 -0700

    nano

---
+++ emails/index.ts
@@ -0,0 +1,42 @@
+import { customAlphabet } from "nanoid"
+import { ReactElement, JSXElementConstructor } from "react"
+import { Resend } from "resend"
+
+export const nanoid = customAlphabet(
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+  7,
+)
+
+export const resend = process.env.RESEND_API_KEY
+  ? new Resend(process.env.RESEND_API_KEY)
+  : null
+
+export const sendEmail = async ({
+  email,
+  subject,
+  react,
+  test,
+}: {
+  email: string
+  subject: string
+  react: ReactElement<any, string | JSXElementConstructor<any>>
+  marketing?: boolean
+  test?: boolean
+}) => {
+  if (!resend) {
+    console.log(
+      "Resend is not configured. You need to add a RESEND_API_KEY in your .env file for emails to work.",
+    )
+    return Promise.resolve()
+  }
+  await resend.emails.send({
+    from: "hello@code-genius.dev",
+    to: test ? "delivered@resend.dev" : email,
+    subject,
+    react,
+    //@ts-ignore
+    headers: {
+      "X-Entity-Ref-ID": nanoid(),
+    },
+  })
+}
