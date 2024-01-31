a2dd7a8665a3c4de94625b6fd5f55b78a086e006
lautapercuspain <lautapercuspain@gmail.com>
2024-01-31 15:45:35 -0700

    welcome email template

--- emails/welcome-email.tsx
+++ emails/welcome-email.tsx
@@ -13,84 +13,84 @@ import {
   Text,
 } from "@react-email/components"
 
-export const DUB_LOGO =
-  "https://public.blob.vercel-storage.com/kmKY9FhOzDRAX28c/logo-1Y8NV0x4Wsy7LzPAYjBmkytJYTMJi0.png"
+// export const CODE_GENIUS_HEADER =
+//   "https://www.code-genius.dev/blog/code-genius-logo.png"
 
-export const DUB_THUMBNAIL =
-  "https://public.blob.vercel-storage.com/kmKY9FhOzDRAX28c/thumbnail-wU82A4LTeJMXrygW1ZR6O36k3edeJf.png"
+export const LOGO_THUMBNAIL = "https://www.code-genius.dev/logo/code-genius.png"
 
 export default function WelcomeEmail({ name = "" }: { name: string | null }) {
   return (
     <Html>
       <Head />
-      <Preview>Welcome to Code Genius</Preview>
+      <Preview>Welcome {`${name}`}</Preview>
       <Tailwind>
         <Body className="mx-auto my-auto bg-white font-sans">
           <Container className="mx-auto my-10 max-w-[500px] rounded border border-solid border-gray-200 px-10 py-5">
-            <Section className="mt-8">
-              <Img
-                src={DUB_LOGO}
-                width="40"
-                height="40"
-                alt="Code Genius"
-                className="mx-auto my-0"
-              />
-            </Section>
             <Heading className="text-xl mx-0 my-7 p-0 text-center font-semibold text-black">
-              Welcome to Code Genius
+              Welcome {`${name}`}
             </Heading>
             <Section className="my-8">
               <Img
-                src={DUB_THUMBNAIL}
+                src={LOGO_THUMBNAIL}
                 alt="Code Genius"
                 className="max-w-[500px]"
               />
             </Section>
             <Text className="text-sm leading-6 text-black">
-              Thanks for signing up{name && `, ${name}`}!
+              Thanks for signing up!
             </Text>
             <Text className="text-sm leading-6 text-black">
-              My name is Steven, and I'm the founder of Code Genius - the link
-              management tool for modern marketing teams. I'm excited to have
-              you on board!
+              I'm Paul, the founder of Code Genius, the handy developer tool for
+              the AI era. I'm thrilled to welcome you on board!
             </Text>
             <Text className="text-sm leading-6 text-black">
-              Here are a few things you can do:
+              At Code Genius, our mission is to enhance your developer
+              experience and boost your efficiency, providing you with the tools
+              and resources you need to succeed. Whether you're a seasoned coder
+              or just starting out, we're here to support you every step of the
+              way.
+            </Text>
+            <Text className="text-sm leading-6 text-black">
+              Here are a few things you can do right away:
             </Text>
             <Text className="text-sm ml-1 leading-4 text-black">
-              ◆ Create a{" "}
+              <Text className="font-bold">◆ Chat with Code Genius: </Text>
+              Start asking anything programming-related on our
               <Link
-                href="https://app.Code Genius.sh/links"
+                href="https://code-genius.dev/code-chat"
                 className="text-blue-600 font-medium no-underline"
               >
-                Code Genius.sh short link
-              </Link>
-            </Text>
-            <Text className="text-sm ml-1 leading-4 text-black">
-              ◆ Create a{" "}
-              <Link
-                href="https://app.Code Genius.sh"
-                className="text-blue-600 font-medium no-underline"
-              >
-                new project
+                {" "}
+                Chat page.
               </Link>{" "}
-              and add your custom domain
             </Text>
             <Text className="text-sm ml-1 leading-4 text-black">
-              ◆ Follow us on{" "}
+              <Text className="font-bold">◆ Premium Membership:</Text>
+              Explore the benefits of becoming a premium member on our
               <Link
-                href="https://twitter.com/dubdotsh"
+                href="https://code-genius.dev/pricing"
                 className="text-blue-600 font-medium no-underline"
               >
-                Twitter
-              </Link>
+                {" "}
+                Pricing page
+              </Link>{" "}
+              for exclusive access to premium functionalities.
             </Text>
+
             <Text className="text-sm leading-6 text-black">
-              Let me know if you have any questions or feedback. I'm always
-              happy to help!
+              Have questions or feedback? I'm here to help! Send us an email to{" "}
+              <Link
+                href="mailto:support@code-genius.dev"
+                className="text-blue-600 font-medium no-underline"
+              >
+                support@code-genius.dev{" "}
+              </Link>
             </Text>
             <Text className="text-sm font-light leading-6 text-gray-400">
-              Steven from Code Genius
+              Happy coding,
+            </Text>
+            <Text className="text-sm font-light leading-6 text-gray-400">
+              Paul
             </Text>
           </Container>
         </Body>
