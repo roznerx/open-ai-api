ef06d5f0591f1764a01aba09dd403dd433120305
lautapercuspain <lautapercuspain@gmail.com>
2024-01-31 15:44:15 -0700

    adding email route

---
+++ app/api/send/route.ts
@@ -0,0 +1,29 @@
+import WelcomeEmail from "emails/welcome-email"
+import { NextRequest, NextResponse } from "next/server"
+import { Resend } from "resend"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function POST(req: NextRequest) {
+  const bodyRequest = await req.json()
+
+  try {
+    const { data } = await resend.emails.send({
+      from: "delivered@resend.dev",
+      to: bodyRequest.email,
+      subject: bodyRequest.isNewUser
+        ? "Welcome to Code Genius"
+        : bodyRequest.subjet,
+      react: WelcomeEmail({
+        name: "Lautaro",
+      }),
+      headers: {
+        "X-Entity-Ref-ID": Math.random().toString(),
+      },
+    })
+
+    return NextResponse.json(data)
+  } catch (error) {
+    return NextResponse.json({ error })
+  }
+}
