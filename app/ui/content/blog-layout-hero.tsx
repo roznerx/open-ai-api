d4771a704c829039be8cbbdf6cde886045671d98
lautapercuspain <lautapercuspain@gmail.com>
2024-02-08 16:27:27 -0700

    author

--- app/ui/content/blog-layout-hero.tsx
+++ app/ui/content/blog-layout-hero.tsx
@@ -12,15 +12,15 @@ export default function BlogLayoutHero() {
   const data = BLOG_CATEGORIES.find((category) => category.slug === slug)
 
   return (
-    <MaxWidthWrapper className="h-52 bg-gradient-to-r from-mint to-blue pb-20 sm:h-52">
+    <MaxWidthWrapper className="h-auto bg-blue pb-20 ">
       <div className="mt-12 flex flex-col justify-center">
         <div className="flex items-center justify-center">
-          <h2 className="mt-7 font-sans text-3xl font-semibold text-white sm:text-4xl">
-            {data?.title || "Our latest news and updates"}
+          <h2 className="my-7 text-center font-sans text-3xl font-semibold text-white sm:text-4xl">
+            {data?.title || "Follow the AI latest news"}
           </h2>
         </div>
         <div className="flex items-center justify-center text-white">
-          <h3 className="my-2 w-4/5 text-center text-2xl sm:text-3xl">
+          <h3 className=" w-4/5 text-center text-2xl sm:text-3xl">
             {data?.description ||
               "Discover the most recent updates regarding artificial intelligence"}
           </h3>
