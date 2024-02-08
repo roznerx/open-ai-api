2a7aab4052dfdd4f7af2aa162d707bf6e1518ca8
lautapercuspain <lautapercuspain@gmail.com>
2024-02-08 15:05:23 -0700

    blog improvements

--- app/(blog-post)/blog/[slug]/page.tsx
+++ app/(blog-post)/blog/[slug]/page.tsx
@@ -125,7 +125,7 @@ export default async function BlogArticle({
               </Link>
             </div>
           </div>
-          <div className="text-xs sticky top-8 col-span-1 hidden flex-col divide-y divide-purple-500  self-start bg-purple-900 sm:flex">
+          <div className="text-xs  top-8 col-span-1 -ml-[40px] hidden flex-col divide-y divide-gray-300 self-start rounded-xl  bg-purple-900 pl-5 sm:flex">
             <div className="text-xs flex flex-col space-y-4 py-5">
               <p className="text-sm text-gray-200">Written by</p>
               <Author username={data.author} />
