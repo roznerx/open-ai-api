c2314c41bf495ef2367501fb5a4fa89add857ccb
lautapercuspain <lautapercuspain@gmail.com>
2024-02-18 08:56:17 -0700

    leet code page

---
+++ app/code-chat/leet-code/page.tsx
@@ -0,0 +1,27 @@
+import { authOptions } from "pages/api/auth/[...nextauth]"
+import { getServerSession } from "next-auth"
+import Client from "./client"
+import { headers } from "next/headers"
+import { getDictionary } from "app/(lang)/dictionaries"
+
+export const metadata = {
+  title: "Code Chat",
+  description: "Code Chat is a fun way to learn how to code.",
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | undefined }
+}) {
+  const session = await getServerSession(authOptions)
+  const headersList = headers()
+  const lang = headersList.get("accept-language")?.split(",")[0].substring(0, 2)
+  const dictionary = await getDictionary(lang)
+
+  return (
+    <main className=" flex h-screen w-full items-start justify-center bg-[radial-gradient(at_bottom_center,_var(--tw-gradient-stops))] from-black to-violet-950">
+      <Client session={session} translations={dictionary?.chat} />
+    </main>
+  )
+}
