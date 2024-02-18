91b19fb2b718bccca2fa52d594aca4c85511ca23
lautapercuspain <lautapercuspain@gmail.com>
2024-02-18 08:59:14 -0700

    improve ux

--- app/home/ChatContainer.tsx
+++ app/home/ChatContainer.tsx
@@ -10,9 +10,9 @@ export default function ChatContainer({ messages, useFullHeight }: any) {
       <div
         ref={chatContainerRef}
         className={`h-auto overflow-y-scroll ${
-          useFullHeight ? "max-h-[75vh] sm:max-h-[75vh]" : "h-auto"
-        } mt-2 w-[90vw] rounded-md bg-transparent font-sans text-white
-       sm:mt-2`}
+          useFullHeight ? "max-h-[75vh] sm:max-h-[65vh]" : "h-auto"
+        } mt-2 w-[60vw] rounded-md bg-transparent font-sans text-white
+       sm:-mt-12`}
       >
         {messages}
       </div>
