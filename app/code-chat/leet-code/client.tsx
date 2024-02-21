b6fa918e65fbdc2d5a5374a2c022e321c637bb61
lautapercuspain <lautapercuspain@gmail.com>
2024-02-20 21:58:14 -0700

    cat list

--- app/code-chat/leet-code/client.tsx
+++ app/code-chat/leet-code/client.tsx
@@ -5,6 +5,22 @@ import Chat from "app/components/shared/Chat"
 import InputChat from "app/components/shared/InputChat"
 import { useEffect, useState } from "react"
 
+const categories = [
+  "LinkedIn List",
+  "Queues",
+  "Stacks",
+  "Hash Tables",
+  "Heaps",
+  "Recursion",
+  "Trees",
+  "Tries",
+  "Binary Search",
+  "Dynamic Programming",
+  "Graph Theory",
+  "DFS Traversal",
+  "BFS Traversal",
+  "Dijkstra Algorithm",
+]
 export default function Client({
   session,
   translations,
@@ -40,29 +56,42 @@ export default function Client({
   }, [questionPrompt, setInput, setQuestionPrompt])
 
   return (
-    <div className="flex h-full justify-start">
-      <div className="absolute inset-0 pt-14 text-3xl text-celeste">
-        <h1 className="mx-auto flex w-[75%] flex-row justify-center text-center sm:w-full">
+    <>
+      <div className="absolute inset-0 my-16 ">
+        <h1 className="mx-auto flex w-[65%] flex-row justify-center text-center text-2xl text-celeste sm:w-full">
           Mastering LeetCode Problems Simplified
         </h1>
+        {/* <ul className="mx-auto flex h-10 w-[60%] flex-row flex-nowrap gap-1 space-x-8  overflow-x-auto bg-red-200">
+          {categories.map((item) => (
+            <li
+              key={item}
+              className="flex-shrink-0 rounded-lg border border-gray-300 bg-slate-100 p-2"
+            >
+              <Link
+                className="h-auto w-auto cursor-pointer underline"
+                href={`?cat=${item}`}
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul> */}
       </div>
-      <div className="flex justify-center">
-        <Chat
-          messages={messages.slice(1)}
-          setQuestionPrompt={setQuestionPrompt}
-          translations={translations}
-          setInput={setInput}
-          userName={userName}
-        />
-        <InputChat
-          reload={reload}
-          initialQuery={initialQuery}
-          translations={translations}
-          inputValue={inputValue}
-          handleInputChange={handleInputChange}
-          handleSubmit={handleSubmit}
-        />
-      </div>
-    </div>
+      <Chat
+        messages={messages.slice(1)}
+        setQuestionPrompt={setQuestionPrompt}
+        translations={translations}
+        setInput={setInput}
+        userName={userName}
+      />
+      <InputChat
+        reload={reload}
+        initialQuery={initialQuery}
+        translations={translations}
+        inputValue={inputValue}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+      />
+    </>
   )
 }
